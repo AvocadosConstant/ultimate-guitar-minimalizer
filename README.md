6818866877726bfbef6d151bdeaa428e81f389b0
Tim <thung1@binghamton.edu>
2017-05-31 16:37:41 -0400

    Remove egregious bad formatting

--- README.md
+++ README.md
@@ -2,21 +2,21 @@
 If you've ever looked for tabs online, you've had to endure Ultimate Guitar's horrendous website.
 This chrome extension gets rid of all the clutter and shows you what you care about.
 
-### Try it out!
+## Try it out!
 1. Clone the repo
 2. Go to your chrome://extensions/
 3. Click "Load unpacked extension"
 4. Navigate to the repo and select the ext/ directory
 
-### Functionality
-##### Implemented
+## Functionality
+### Implemented
 - Purge annoying clutter
     - phone ad
     - video popup
     - sidebar
     - comments
     
-##### To-Do
+### To-Do
 - Inject minimalist styling
 - Reduce header to bare minimum
     - name of piece
--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,35 +1,32 @@
 chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-        if (document.readyState === "complete") {
-            clearInterval(readyStateCheckInterval);
+  var readyStateCheckInterval = setInterval(function() {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
 
-            // This part of the script triggers when page is done loading
-            
-            var clutter = [
-                "t_bl", 
-                "p_h", 
-                "t_comments",
-                "separator",
-                "comment_box_main",
-                "comment_box_h_main",
-                "t_hs", 
-                "cm_cnt",
-                "bdrunion",
-                "head_ad_UG",
-                "vote-success",     //  voting bullshit
-                "fb-meta"           //  social media bullshit
-            ]
+      var clutter = [
+          "t_bl",
+          "p_h",
+          "t_comments",
+          "separator",
+          "comment_box_main",
+          "comment_box_h_main",
+          "t_hs",
+          "cm_cnt",
+          "bdrunion",
+          "head_ad_UG",
+          "vote-success",     //  voting bullshit
+          "fb-meta"           //  social media bullshit
+      ]
 
-            clutter.forEach(function(name){
-                [].forEach.call(document.getElementsByClassName(name), function(entry) {
-                    entry.remove();
-                });
-            });
+      clutter.forEach(function(name){
+          [].forEach.call(document.getElementsByClassName(name), function(entry) {
+              entry.remove();
+          });
+      });
 
-            var ads = document.getElementById("advanced_search");
-            if(ads) ads.remove();
-            
-            //chrome.tabs.executeScript(null, {code:"document.body.style.backgroundColor='#FFF'"});
-}
-	}, 10);
+      var ads = document.getElementById("advanced_search");
+      if(ads) ads.remove();
+      //chrome.tabs.executeScript(null, {code:"document.body.style.backgroundColor='#FFF'"});
+    }
+  }, 10);
 });
